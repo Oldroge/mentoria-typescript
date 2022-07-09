078b11afe623a39ec5d885cef3a408b0c593d00a
oldroge <rogeriomrjr@gmail.com>
2022-07-09 15:25:04 -0300

    Feito desafio 2

--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,42 @@ employeeTest.name = "John";
 
 console.log("Desafio 1:");
 console.log(employeeTest);
+
+// Desafio 2:
+console.log("Desafio2:")
+let pessoaTest1: {nome: string, idade: number, profissao: string} = {
+    nome: "",
+    idade: 0,
+    profissao: ""
+};
+
+pessoaTest1.nome = "maria";
+pessoaTest1.idade = 29;
+pessoaTest1.profissao = "atriz"
+console.log(pessoaTest1);
+
+let pessoaTest2: {nome: string, idade: number, profissao: string} = {
+    nome: "",
+    idade: 0,
+    profissao: ""
+}
+pessoaTest2.nome = "roberto";
+pessoaTest2.idade = 19;
+pessoaTest2.profissao = "Padeiro";
+console.log(pessoaTest2);
+
+
+let pessoaTest3: {nome: string, idade: number, profissao: string} = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+console.log(pessoaTest3);
+
+
+let pessoaTest4: {nome: string, idade: number, profissao: string} = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
+console.log(pessoaTest4);
